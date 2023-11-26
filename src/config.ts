9d28e41c9f0013838319076f1453f26fedeb569e
장호빈 <devhobin@gmail.com>
2023-11-26 18:43:14 +0900

    chore: 기본 스타일 작업

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,16 @@
 import type { Site, SocialObjects } from './types'
 
 export const SITE: Site = {
-  website: 'https://astro-paper.pages.dev/', // replace this with your deployed domain
-  author: 'Sat Naing',
-  desc: 'A minimal, responsive and SEO-friendly Astro blog theme.',
-  title: 'AstroPaper',
+  website: 'https://dev-hobin.github.io/',
+  author: 'Hobin Jang',
+  desc: '개발자로서 여러 생각을 기록합니다',
+  title: '장호빈',
   ogImage: 'astropaper-og.jpg',
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 10,
 }
 
-export const LOCALE = ['en-EN'] // set to [] to use the environment default
+export const LOCALE = ['ko']
 
 export const LOGO_IMAGE = {
   enable: false,
@@ -22,122 +22,20 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: 'Github',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: ` ${SITE.title} on Github`,
-    active: true,
-  },
-  {
-    name: 'Facebook',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on Facebook`,
-    active: true,
-  },
-  {
-    name: 'Instagram',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on Instagram`,
+    href: 'https://github.com/dev-hobin',
+    linkTitle: ` ${SITE.title}의 깃허브`,
     active: true,
   },
   {
     name: 'LinkedIn',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on LinkedIn`,
+    href: 'https://www.linkedin.com/in/janghobin',
+    linkTitle: `${SITE.title}의 링크드인`,
     active: true,
   },
   {
     name: 'Mail',
-    href: 'mailto:yourmail@gmail.com',
-    linkTitle: `Send an email to ${SITE.title}`,
-    active: false,
-  },
-  {
-    name: 'Twitter',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on Twitter`,
-    active: false,
-  },
-  {
-    name: 'Twitch',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on Twitch`,
-    active: false,
-  },
-  {
-    name: 'YouTube',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on YouTube`,
-    active: false,
-  },
-  {
-    name: 'WhatsApp',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on WhatsApp`,
-    active: false,
-  },
-  {
-    name: 'Snapchat',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on Snapchat`,
-    active: false,
-  },
-  {
-    name: 'Pinterest',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on Pinterest`,
-    active: false,
-  },
-  {
-    name: 'TikTok',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on TikTok`,
-    active: false,
-  },
-  {
-    name: 'CodePen',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on CodePen`,
-    active: false,
-  },
-  {
-    name: 'Discord',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on Discord`,
-    active: false,
-  },
-  {
-    name: 'GitLab',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on GitLab`,
-    active: false,
-  },
-  {
-    name: 'Reddit',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on Reddit`,
-    active: false,
-  },
-  {
-    name: 'Skype',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on Skype`,
-    active: false,
-  },
-  {
-    name: 'Steam',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on Steam`,
-    active: false,
-  },
-  {
-    name: 'Telegram',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on Telegram`,
-    active: false,
-  },
-  {
-    name: 'Mastodon',
-    href: 'https://github.com/satnaing/astro-paper',
-    linkTitle: `${SITE.title} on Mastodon`,
-    active: false,
+    href: 'mailto:devhobin@gmail.com',
+    linkTitle: `${SITE.title}에게 이메일 보내기`,
+    active: true,
   },
 ]
